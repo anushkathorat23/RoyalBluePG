be447359b58cd550ee951bd24c3a83a2cf1c4187
anushkathorat23 <anushkathorat.23@gmail.com>
2026-07-16 17:01:06 +0530

    Delete Royalbluepg-main directory

--- Royalbluepg-main/royal-blue-main/server/routes/contentRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getContent, updateContent } = require('../controllers/contentController');
-const { protect } = require('../middleware/auth');
-
-router.get('/', getContent);
-router.put('/', protect, updateContent);
-
-module.exports = router;
