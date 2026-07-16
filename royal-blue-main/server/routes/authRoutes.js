d08c92bcb53c6dc6c2b18361a6aaa6b20635b055
anushkathorat23 <anushkathorat.23@gmail.com>
2026-07-16 17:01:22 +0530

    Delete royal-blue-main directory

--- royal-blue-main/server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { login, getMe } = require('../controllers/authController');
-const { protect } = require('../middleware/auth');
-const { validateLogin } = require('../middleware/validators');
-
-router.post('/login', validateLogin, login);
-router.get('/me', protect, getMe);
-
-module.exports = router;
